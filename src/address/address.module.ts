50b864313df205d592559d087b45de16cb343d63
Arthur <arthurropke@hotmail.com>
2023-06-24 13:14:38 -0300

    feat: insert controller address

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -1,4 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
-@Module({})
+import { AddressController } from './address.controller'
+import { AddressService } from './address.service'
+import { AddressEntity } from './entities/address.entity'
+
+@Module({
+  imports: [TypeOrmModule.forFeature([AddressEntity])],
+  providers: [AddressService],
+  controllers: [AddressController]
+})
 export class AddressModule {}
