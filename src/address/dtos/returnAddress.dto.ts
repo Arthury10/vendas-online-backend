1b094118d3c22559cd885f86673bd8168d72f44b
Arthur <arthurropke@hotmail.com>
2023-06-26 18:27:48 -0300

    feat: insert DTO'S to city and state

--- src/address/dtos/returnAddress.dto.ts
+++ src/address/dtos/returnAddress.dto.ts
@@ -1,13 +1,17 @@
 import { AddressEntity } from '../entities/address.entity'
 
+import { ReturnCityDto } from 'src/city/dto/returnCity.dto'
+
 export class ReturnAddressDto {
   complement: string
   numberAddress: number
   cep: string
+  city?: ReturnCityDto
 
   constructor(address: AddressEntity) {
     this.complement = address.complement
     this.numberAddress = address.numberAddress
     this.cep = address.cep
+    this.city = address.city ? new ReturnCityDto(address.city) : undefined
   }
 }
