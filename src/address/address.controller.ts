83d1b7af1ee3b38a90d5b1ed44419a3e8510b90e
Arthur <arthurropke@hotmail.com>
2023-06-26 22:41:17 -0300

    feat: create decorator userId

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,7 +1,6 @@
 import {
   Body,
   Controller,
-  Param,
   Post,
   UsePipes,
   ValidationPipe
@@ -12,6 +11,7 @@ import { CreateAddressDto } from './dtos/createAddress.dto'
 import { AddressEntity } from './entities/address.entity'
 
 import { Roles } from 'src/decorators/roles.decorator'
+import { UserId } from 'src/decorators/user-id.decorator'
 import { UserType } from 'src/user/enum/user-type.enum'
 
 @Controller('address')
@@ -19,11 +19,11 @@ export class AddressController {
   constructor(private readonly addressService: AddressService) {}
 
   @Roles(UserType.User)
-  @Post('/:userId')
+  @Post()
   @UsePipes(ValidationPipe)
   async createAddress(
     @Body() createAddressDto: CreateAddressDto,
-    @Param('userId') userId: number
+    @UserId() userId: number
   ): Promise<AddressEntity> {
     return this.addressService.createAddress(createAddressDto, userId)
   }
